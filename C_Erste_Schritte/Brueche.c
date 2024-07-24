8b65aa3a7f4a3fc7f5f41b01303344ca216f728b
Peter Loos <peter.loos@gmx.de>
2024-07-24 16:57:30 +0200

    Loesung zu Bruechen

--- C_Erste_Schritte/Brueche.c
+++ C_Erste_Schritte/Brueche.c
@@ -12,9 +12,22 @@ struct Bruch   // Fraction
 
 // erste Funktion / erstes Unterprogramm
 
+// https://de.wikibooks.org/wiki/C%2B%2B-Programmierung/_Br%C3%BCche/_Die_Methoden
+int ggT(int zaehler, int nenner)
+{
+	if (nenner == 0) {
+		return zaehler;
+	}
+	else
+	{
+		return ggT(nenner, zaehler % nenner);
+	}
+}
+
+
 void printBruch(struct Bruch b)
 {
-	printf("%d / %d\n", b.zaehler, b.nenner);
+	printf("%d / %d", b.zaehler, b.nenner);
 }
 
 // 3 mal 1/7 = 3/7
@@ -33,7 +46,20 @@ struct Bruch scalarProdukt (struct Bruch b, int scalar)
 
 struct Bruch addiereBruch(struct Bruch b1, struct Bruch b2)
 {
-	// to be done
+	struct Bruch result;
+
+	int hauptNenner = b1.nenner * b2.nenner;
+
+	result.zaehler = b1.zaehler * b2.nenner + b2.zaehler * b1.nenner;
+	result.nenner = hauptNenner;
+
+	// mit kuerzen
+	int teiler = ggT(result.zaehler, result.nenner);
+
+	result.zaehler = result.zaehler / teiler;
+	result.nenner = result.nenner / teiler; ;
+
+	return result;
 }
 
 // 2. Aufgabe:
@@ -41,7 +67,18 @@ struct Bruch addiereBruch(struct Bruch b1, struct Bruch b2)
 
 struct Bruch multipliziereBruch(struct Bruch b1, struct Bruch b2)
 {
-	// to be done
+	struct Bruch result;
+
+	result.zaehler = b1.zaehler * b2.zaehler;
+	result.nenner = b1.nenner * b2.nenner;
+
+	// mit kuerzen
+	int teiler = ggT(result.zaehler, result.nenner);
+
+	result.zaehler = result.zaehler / teiler;
+	result.nenner = result.nenner / teiler; ;
+
+	return result;
 }
 
 // Sonderaufgabe:
@@ -52,9 +89,33 @@ void test_bruch()
 {
 	struct Bruch b = { 1, 7 };
 	printBruch(b);
+	printf("\n");
 
 	struct Bruch ergebnis;
 	ergebnis = scalarProdukt(b, 3);
 	printBruch(ergebnis);
+	printf("\n");
+
+	// Test der Addition
+	struct Bruch b1 = { 1, 3 };
+	struct Bruch b2 = { 4, 3 };
+	ergebnis = addiereBruch(b1, b2);
+
+	printBruch(b1);
+	printf(" + ");
+	printBruch(b2);
+	printf(" = ");
+	printBruch(ergebnis);
+	printf("\n");
+
+	// Test der Multiplikation
+	ergebnis = multipliziereBruch(b1, b2);
+
+	printBruch(b1);
+	printf(" * ");
+	printBruch(b2);
+	printf(" = ");
+	printBruch(ergebnis);
+	printf("\n");
 }
 
